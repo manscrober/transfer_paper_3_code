033ed2664f3fbaf8f54335d96d8e677a52e63ac0
Robert Manschke <gamingrobert@gmx.de>
2020-05-01 16:12:16 +0200

    added stage1

--- server.js
+++ server.js
@@ -1,56 +1,156 @@
-const express = require('express');
-const app = express();
-const bodyParser=require('body-parser');
+/*
+for data pass all params(even singular strings) as array:
+/data?itemID[]=746142S3&vexID[]=123&vexID[]=123
+*/
+
+
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs')
-const mssql=require('mssql');
-var corsOptions={
-    origin:'http://localhost:4200',
-    optionsSuccessStatus:200
+const fs = require('fs');
+const Connection = require('tedious').Connection;
+const Request = require('tedious').Request;
+const TYPES = require('tedious').TYPES;
+const express = require('express');
+const whitelist = ['http://localhost:4200'];
+
+const app = express();
+var corsOptions = {
+   /* origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    optionsSuccessStatus: 200
+*/
 }
+const config = {
+    server: 'serverhostname', // You can use 'localhost\\instance' to connect to named instance
+    options: {
+        encrypt: true,
+    },
+    authentication:{
+        type:"default",
+        options:{
+            userName: 'username',
+            password: 'password',
+        }
+    }
+}
+
+const filterablequery = "select auftrag,ItemId,C_LFDNR, convert(varchar, datum, 104) datum,"+
+    " convert(varchar,datum, 8) uhrzeit,WorkpieceID,c_Balgnr,convert(varchar,WorkpieceDurability,104) WorkpieceDurability,ManufacturedAt from v_basic_vulk_data " +
+    "where 1=1 and ";
+
+
+
+const columnNames={
+    itemID: "itemID",
+    counter: "c_lfdnr",
+    vexID: "WorkpieceID",
+    orderNum: "auftrag",
+    itemIDSAP: "",
+    partNumSAP: "",
+    vulkID: "c_balgnr",
+    cageID: "",
+    vulkDate: "convert(date,datum)",
+    blankRubberBestBefore: "WorkpieceDurability",
+    blankRubberManufactured: "substring(ManufacturedAt,1,10)",
+}
+
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
-app.use((req,res,next)=>{
-    console.log("getting: " + req.path);
+app.use((req, res, next) => {
+    console.log("getting: " + req.url);
     next();
 })
 
 
-app.get('/docs/:docname',(req,res)=>{
-    var name=req.params.docname.replace(/[/]/g,"");
-    var tempFile="./docs/"+name;
-        fs.readFile(tempFile, (err,data)=>{
-            res.contentType('arraybuffer');
-            res.send(data);
-        });
+app.get('/docs/:docname', (req, res) => {
+    var name = req.params.docname.replace(/[/]/g, "");
+    var tempFile = "./docs/" + name;
+    fs.readFile(tempFile, (err, data) => {
+        res.contentType('arraybuffer');
+        res.send(data);
+    });
 });
-app.get('/data/:name', (req,res)=>{
-    (async function getTestData() {
-        try {
-            let pool = await mssql.connect(config)
-            let result1 = await pool.request()
-                .query('select top 1000 * from v_basic_vulk_data;')
-            mssql.close();
-            res.send(result1);
-        } catch (err) {
-            console.log(err);
-        }
-    })()
-    
+app.get('/data', (req, res) => {
+   getData(req, res);
 });
-const config = {
-    user: 'username',
-    password: 'password',
-    server: 'serverhostname', // You can use 'localhost\\instance' to connect to named instance
-    database: 'databasename',
- 
-    options: {
-        encrypt: false // Use this if you're on Windows Azure
-    }
-}
 
- 
-mssql.on('error', err => {
-    console.log(err);
-})
-app.listen(3000,()=>{console.log('Server started!');});
+function getData(req, res) {
+
+    let params = req.query;
+    let record = [];
+    if (Object.keys(params).length = 0) {
+        res.send({});
+        return;
+    }
+    //e.g.: [itemID[0,1],vexID[0]]=> "(itemID=itemID0 or itemID=itemID1) and vexID=vexID0"
+    let filterstring = Object.keys(params).map(k => //map all keys k of params to 
+        {   
+            let s="("+                                    //(filters[k]+i or filters[k]+(i+1)) and (filters[k+1]+i or ...)...                                       
+            Object.keys(params[k]).map(p=>{
+                        if(params[k][p]!=""){             
+                            return (columnNames[k]+" like @"+k+p);
+                        }else{
+                            return (columnNames[k]+" is null");
+                        }
+                    }  
+                ).join(" or ")                             
+            +")";
+            return s;
+        }                                       
+    ).join(" and ");                            
+    if (filterstring == "") filterstring = " 1=0";//if filterstring is empty filter for false to get an empty result
+    let sqlquery = filterablequery + filterstring;
+    console.log(sqlquery);
+    var request = new Request(sqlquery, function (err, rowCount) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(rowCount + ' rows. '+"exec time:" + (new Date() - start) + "ms");
+        }
+        connection.close();
+    });
+
+    //add parameters with indices
+    //?itemID[]=foo&itemID[]=bar&vexID[]=flip
+    //is added as itemID0=foo,itemID1=bar and vexID0=flip
+    Object.keys(params).forEach(k => Object.keys(params[k]).forEach(p=>{
+        if(params[k][p]!=""){
+            request.addParameter(k+p, TYPES.VarChar, params[k][p])
+        }
+        })
+    
+    );
+    console.log(JSON.stringify(request.parameters));
+
+    request.on('row', function (columns) {
+        let recLength = Object.keys(record).length;
+        record[recLength] = {};
+        columns.forEach((column) => {
+            record[recLength][column.metadata.colName] = column.value;
+        });
+    });
+
+    request.on('requestCompleted', (rowCount, more, rows) => {
+        record = JSON.stringify(record);
+        res.send(record);
+    });
+    let start= new Date();
+    var connection = new Connection(config);
+    connection.on('connect', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Connected');
+        }
+        
+        connection.execSql(request);
+    }); 
+}
+app.listen(3000, () => {
+    console.log('Server started!');
+});
